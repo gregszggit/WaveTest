094d6b95f4e8f2002f634dcc0b72b02a392d0949
gregszggit <greg@solitonzgames.com>
2020-03-08 08:04:57 -0700

    Initial commit

---
+++ Assets/RectanglePlane.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Ignore or delete this, was starting to write a more generalized version of the other class
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshCollider))]
+public class RectanglePlane: MonoBehaviour
+{
+	public float x, y, w, h;
+	public int verticesPerUnit;
+
+	private Mesh _mesh;
+	private Vector2 _offset, _size;
+	private float _verticesPerUnit;
+	private MeshFilter _meshFilter;
+	private MeshCollider _meshCollider;
+
+	private void Start()
+	{
+		_meshFilter = GetComponent<MeshFilter>();
+		_meshCollider = GetComponent<MeshCollider>();
+	}
+
+	private int[] CalculateTriangles()
+	{
+		return new int[0];
+	}
+
+	private void Update()
+	{
+		
+	}
+}
